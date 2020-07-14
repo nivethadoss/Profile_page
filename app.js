ac2189d0be778462b1968dc535ff282b73b6ecda
Nivetha Doss <nivethasujadoss@gmail.com>
2020-07-14 12:43:46 +0200

    added Images

--- app.js
+++ app.js
@@ -2,14 +2,9 @@ var express = require("express");
 var app  = express();
 
 app.set('view engine', 'ejs');
-
+app.use( express.static( "public" ) );
 
 app.get("/", function(req, res){
-    res.render("index")
-});
-
-
-app.get("/template", function(req, res){
     res.render("template")
 });
 
